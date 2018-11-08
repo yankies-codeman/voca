a75619b7c04d2b665f07d5746a5a5e50ca924625
f-yankey <kobidel18@gmail.com>
2018-11-08 08:25:40 +0000

    new services

---
+++ lib/services/message_service.dart
@@ -0,0 +1,18 @@
+
+class MessageService{
+  static  MessageService _instance = new MessageService.internal();
+
+  factory MessageService() {
+    return _instance;
+  }
+
+   get getInstance{
+    if (_instance != null) {
+      return _instance;
+    }
+    _instance =  new MessageService.internal();
+    return _instance;
+  }
+
+  MessageService.internal();
+}
